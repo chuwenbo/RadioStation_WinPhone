1634d99fbfa99ece68ba2c0379e347a031cc8e07
褚文波 <idiot@live.cn>
2015-04-05 18:46:40 +0800

    test isolated storage

--- Chu.RadioStation.DataAccess/RadioContext.cs
+++ Chu.RadioStation.DataAccess/RadioContext.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chu.RadioStation.DataAccess
+namespace Chu.RadioStation.LocalDatabase
 {
-    public class Class1
+    class RadioContext
     {
     }
 }
